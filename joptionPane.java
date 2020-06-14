3410d94a2fff5a3e6c121b0c27312f4491d5ed3e
shreesha08 <53050487+shreesha08@users.noreply.github.com>
2020-06-14 22:47:23 +0530

    Create joptionPane.java

---
+++ joptionPane.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author lenovo
+ */
+public class JoptionPane {
+       static void showMessageDialog(Object object, String incorrect_Username) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    static void showMessageDialog(Object object, Exception e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
+
+    
+
